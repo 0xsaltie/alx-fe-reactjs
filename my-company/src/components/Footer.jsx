42e75a36cdd555e8afbc613ef461153e5e8b619e
0xsaltie <elmulayhiyyu@gmail.com>
2025-11-10 03:39:56 +0100

    latest

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "1rem", backgroundColor: "#f1f1f1" }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-company/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Welcome to My Company</h1>
+      <p>We provide top-notch services to help your business grow.</p>
+    </div>
+  );
+}
+
+export default Home;
