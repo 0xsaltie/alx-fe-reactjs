269f0c400a25466cfd2cf39409b540b0440bd0e1
0xsaltie <elmulayhiyyu@gmail.com>
2025-11-16 18:55:50 +0100

    new

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,41 +1,39 @@
-import { useParams, Link } from 'react-router-dom';
-import useRecipeStore from '../store/recipeStore';
-import DeleteRecipeButton from './DeleteRecipeButton';
-
+import { useParams, Link } from "react-router-dom";
+import useRecipeStore from "../store/recipeStore";
+import DeleteRecipeButton from "./DeleteRecipeButton";
 
 const RecipeDetails = () => {
-const { id } = useParams();
-const recipe = useRecipeStore((state) => state.recipes.find((r) => r.id === id));
+  const { id } = useParams();
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((r) => r.id === id)
+  );
 
+  if (!recipe)
+    return (
+      <div>
+        <p>Recipe not found.</p>
+        <Link to="/">Back to recipes</Link>
+      </div>
+    );
 
-if (!recipe) return (
-<div>
-<p>Recipe not found.</p>
-<Link to="/">Back to recipes</Link>
-</div>
-);
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
 
+      <div style={{ marginTop: "1rem" }}>
+        <Link to={`/recipes/${recipe.id}/edit`} style={{ marginRight: "1rem" }}>
+          Edit
+        </Link>
 
-return (
-<div>
-<h1>{recipe.title}</h1>
-<p>{recipe.description}</p>
+        <DeleteRecipeButton id={recipe.id} />
+      </div>
 
-
-<div style={{ marginTop: '1rem' }}>
-<Link to={`/recipes/${id}/edit`} style={{ marginRight: '1rem' }}>
-Edit
-</Link>
-<DeleteRecipeButton id={id} />
-</div>
-
-
-<div style={{ marginTop: '1rem' }}>
-<Link to="/">Back to recipes</Link>
-</div>
-</div>
-);
+      <div style={{ marginTop: "1rem" }}>
+        <Link to="/">Back to recipes</Link>
+      </div>
+    </div>
+  );
 };
-
 
 export default RecipeDetails;
