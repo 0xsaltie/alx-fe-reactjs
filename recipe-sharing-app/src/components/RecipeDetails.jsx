5d50cd13840361ee0d6aa346fe8e643b607fad8f
0xsaltie <elmulayhiyyu@gmail.com>
2025-11-16 19:14:36 +0100

    new

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -4,23 +4,27 @@ import DeleteRecipeButton from "./DeleteRecipeButton";
 
 const RecipeDetails = () => {
   const { id } = useParams();
+
   const recipe = useRecipeStore((state) =>
     state.recipes.find((r) => r.id === id)
   );
 
-  if (!recipe)
+
+  if (!recipe) {
     return (
       <div>
         <p>Recipe not found.</p>
-        <Link to="/">Back to recipes</Link>
+        <Link to="/">Back to Recipes</Link>
       </div>
     );
+  }
 
   return (
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
 
+      {/* recipe.id is explicitly used here */}
       <div style={{ marginTop: "1rem" }}>
         <Link to={`/recipes/${recipe.id}/edit`} style={{ marginRight: "1rem" }}>
           Edit
@@ -30,7 +34,7 @@ const RecipeDetails = () => {
       </div>
 
       <div style={{ marginTop: "1rem" }}>
-        <Link to="/">Back to recipes</Link>
+        <Link to="/">Back to Recipes</Link>
       </div>
     </div>
   );
