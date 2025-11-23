e9b3f15ee8c10e1b6937ddc0c5ae6922adcb8485
0xsaltie <elmulayhiyyu@gmail.com>
2025-11-23 08:23:25 +0100

    modified

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -10,6 +10,16 @@ set((state) => ({ recipes: [...state.recipes, newRecipe] })),
 
 
 setRecipes: (recipes) => set({ recipes }),
+
+
+updateRecipe: (updatedRecipe) =>
+set((state) => ({
+recipes: state.recipes.map((r) => (r.id === updatedRecipe.id ? { ...r, ...updatedRecipe } : r)),
+})),
+
+
+deleteRecipe: (id) =>
+set((state) => ({ recipes: state.recipes.filter((r) => r.id !== id) })),
 }));
 
 
