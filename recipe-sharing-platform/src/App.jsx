b2309775816f50ed4e94dfadb2c9795f1c8a7f79
0xsaltie <elmulayhiyyu@gmail.com>
2025-12-07 20:28:21 +0100

    new

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-red-500 text-white text-4xl flex items-center justify-center">
+      TAILWIND TEST
+    </div>
+  );
+}
+
